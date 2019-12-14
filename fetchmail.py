c937297aed99a638092064b439feebf1d96af959
XtraLarge <eMail@WilliWerres.de>
2019-12-14 10:33:22 +0100

    Update fetchmail.py

--- fetchmail.py
+++ fetchmail.py
@@ -11,7 +11,6 @@ import requests
 
 FETCHMAIL = """
 fetchmail -N \
-    --sslcertck --sslcertpath /etc/ssl/certs \
     -f {}
 """
 
@@ -44,49 +43,49 @@ def fetchmail(fetchmailrc):
 
 
 def run(debug):
-    fetches = requests.get("http://admin/internal/fetch").json()
+    source_user  = os.getenv('SOURCE_USER', '')
+    source_pass  = os.getenv('SOURCE_PASS', '')
+    source_host  = os.getenv('SOURCE_HOST', '')
+    source_port  = os.getenv('SOURCE_PORT', '')
+    source_proto = os.getenv('SOURCE_PROTOCOL', '')
+    dest_user    = os.getenv('DEST_USER', '')
+    dest_pass    = os.getenv('DEST_PASS', '')
+    dest_host    = os.getenv('DEST_HOST', '')
+    dest_port    = os.getenv('DEST_PORT', '')
+    options      = os.getenv('OPTIONS', '')
+    
     smtphost, smtpport = extract_host_port(os.environ.get("HOST_SMTP", "smtp"), None)
     if smtpport is None:
         smtphostport = smtphost
     else:
         smtphostport = "%s/%d" % (smtphost, smtpport)
-    for fetch in fetches:
-        fetchmailrc = ""
-        options = "options antispam 501, 504, 550, 553, 554"
-        options += " sslmode wrapped" if fetch["tls"] else ""
-        options += " keep" if fetch["keep"] else " fetchall"
-        fetchmailrc += RC_LINE.format(
-            user_email=escape_rc_string(fetch["user_email"]),
-            protocol=fetch["protocol"],
-            host=escape_rc_string(fetch["host"]),
-            port=fetch["port"],
-            smtphost=smtphostport,
-            username=escape_rc_string(fetch["username"]),
-            password=escape_rc_string(fetch["password"]),
-            options=options
-        )
-        if debug:
-            print(fetchmailrc)
-        try:
-            print(fetchmail(fetchmailrc))
-            error_message = ""
-        except subprocess.CalledProcessError as error:
-            error_message = error.output.decode("utf8")
-            # No mail is not an error
-            if not error_message.startswith("fetchmail: No mail"):
-                print(error_message)
-            user_info = "for %s at %s" % (fetch["user_email"], fetch["host"])
-            # Number of messages seen is not a error as well
-            if ("messages" in error_message and
-                    "(seen " in error_message and
-                    user_info in error_message):
-                print(error_message)
-        finally:
-            requests.post("http://admin/internal/fetch/{}".format(fetch["id"]),
-                json=error_message.split("\n")[0]
-            )
-
-
+    fetchmailrc += RC_LINE.format(
+        user_email=escape_rc_string(fetch["user_email"]),
+        protocol=fetch["protocol"],
+        host=escape_rc_string(fetch["host"]),
+        port=fetch["port"],
+        smtphost=smtphostport,
+        username=escape_rc_string(fetch["username"]),
+        password=escape_rc_string(fetch["password"]),
+        options=options
+    )
+    if debug:
+        print(fetchmailrc)
+    try:
+        print(fetchmail(fetchmailrc))
+        error_message = ""
+    except subprocess.CalledProcessError as error:
+        error_message = error.output.decode("utf8")
+        # No mail is not an error
+        if not error_message.startswith("fetchmail: No mail"):
+            print(error_message)
+        user_info = "for %s at %s" % (fetch["user_email"], fetch["host"])
+        # Number of messages seen is not a error as well
+        if ("messages" in error_message and
+                "(seen " in error_message and
+                user_info in error_message):
+            print(error_message)
+        
 if __name__ == "__main__":
     while True:
         time.sleep(int(os.environ.get("FETCHMAIL_DELAY", 60)))
